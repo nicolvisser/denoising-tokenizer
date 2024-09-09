f2c6efb9a0a88db698fdb0b446aa6306865142c3
nicolvisser <nicolvisser@yahoo.com>
2024-09-09 08:20:37 +0200

    add torch hub loading

---
+++ hubconf.py
@@ -0,0 +1,33 @@
+dependencies = ["torch", "torchaudio", "xformers", "simple_parsing"]
+
+import requests
+import torch
+from torch.nn.modules.utils import consume_prefix_in_state_dict_if_present
+
+from denoizr.tokenizer import TokenizerModel, TokenizerModelArgs
+
+
+def _tokenizer(
+    tag: str,
+    progress: bool = True,
+) -> TokenizerModel:
+    response = requests.get(
+        f"https://github.com/nicolvisser/denoising-tokenizer/releases/download/{tag}/model_config.json",
+    )
+    if response.status_code == 200:
+        config = response.json()
+    else:
+        raise Exception(f"Failed to fetch JSON file: {response.status_code}")
+
+    model_args = TokenizerModelArgs.from_dict(config)
+    model = TokenizerModel(model_args)
+    checkpoint = torch.hub.load_state_dict_from_url(
+        f"https://github.com/nicolvisser/denoising-tokenizer/releases/download/{tag}/best.pth",
+        progress=progress,
+    )
+    model.load_state_dict(checkpoint)
+    return model
+
+
+def tokenizer(tag=str, progress: bool = True) -> TokenizerModel:
+    return _tokenizer(tag, progress)
